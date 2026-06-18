779b97955e50fae042e41daef79a9494c7fd7ff6
BOS-OMEGA <paisabrazilfl@gmail.com>
2026-06-18 18:09:44 -0400

    Initial commit

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+
+class SnakeGame(App):
+    def build(self):
+        return Label(text='Hello, World!')
+
+if __name__ == '__main__':
+    SnakeGame().run()
